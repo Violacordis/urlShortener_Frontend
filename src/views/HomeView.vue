f05fcf95cb8d0e6aab3f946b39e64e8f235b45d6
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-03 21:18:00 +0100

    docs: add links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,28 @@
-<template>
-<header class="grid grid-col-5 p-2 gap-1 items-center">
-  <h1>Logo</h1>
-  <form class="p-1 row-start-2 col-span-5 border-2 border-current rounded-3xl md:col-start-2 md:col-span-3 md:row-start-1">
-    <input type="url" placeholder="Enter the link here" class="rounded-3xl w-3/4 p-2">
-    <input type="submit" value="Shorten Now" class="col-start-5 w-1/4 rounded-3xl bg-primary-blue text-white p-2">
-  </form>
-  <nav class="col-start-5 justify-self-end">
-    <ul class="flex items-center gap-2">
-      <li>user</li>
-      <li>logout</li>
-    </ul>
-  </nav>
-</header>
+<script setup lang="ts">
 
+
+</script>
+<template>
+  <header class="grid grid-col-5 p-3 md:py-8 md:px-5 gap-1 items-center mb-4">
+    <h1>Logo</h1>
+    <form
+      class="mt-2 row-start-2 col-span-5 border-2 border-current rounded-3xl md:col-start-2 md:col-span-3 md:row-start-1 md:mt-0">
+      <input type="url" placeholder="Enter the link here" class="rounded-3xl w- w-8/12 p-2">
+      <input type="submit" value="Shorten Now" class="col-start-5  w-4/12 rounded-3xl bg-primary-blue text-white p-2">
+    </form>
+    <nav class="col-start-5 justify-self-end">
+      <ul class="flex items-center gap-2">
+        <li>user</li>
+        <li>logout</li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <ul class="flex items-center justify-center gap-5 py-4 px-5 bg-slate-700 text-white">
+      <li>History</li>
+      <li>Statistics</li>
+      <li>Click Steam</li>
+      <li>Settings</li>
+    </ul>
+  </main>
 </template>
