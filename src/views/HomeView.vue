a307bdf1a86df24fed8aa7ebcac146f6064d6e1c
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-03 20:43:14 +0100

    docs: responsive header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <main>
-   <h1>Hello</h1>
-  </main>
+<header class="grid grid-col-5 p-2 gap-1 items-center">
+  <h1>Logo</h1>
+  <form class="p-1 row-start-2 col-span-5 border-2 border-current rounded-3xl md:col-start-2 md:col-span-3 md:row-start-1">
+    <input type="url" placeholder="Enter the link here" class="rounded-3xl w-3/4 p-2">
+    <input type="submit" value="Shorten Now" class="col-start-5 w-1/4 rounded-3xl bg-primary-blue text-white p-2">
+  </form>
+  <nav class="col-start-5 justify-self-end">
+    <ul class="flex items-center gap-2">
+      <li>user</li>
+      <li>logout</li>
+    </ul>
+  </nav>
+</header>
+
 </template>
