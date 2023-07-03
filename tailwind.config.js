05c7dc5519bc6189834e6548a18873391bda64c3
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-03 18:44:17 +0100

    docs: remove scaffold file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [ "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}" ];
 export const theme = {
-  extend: {},
-};
+  color: {
+    pink: '#EB568E',
+    blue: '#144EE3',
+    black: '#0B101B',
+    grey: '#181E29',
+    lite: 'C9CED6'
+  },
+  extend: {}
+}
 export const plugins = [];
 
