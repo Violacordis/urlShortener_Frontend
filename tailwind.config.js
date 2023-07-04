37276b99b64e00a96136ed1ae0e4e67044acf81d
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-04 12:56:35 +0100

    add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [ "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}" ];
+export const content = ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
 export const theme = {
   extend: {
     colors: {
@@ -8,10 +8,11 @@ export const theme = {
       'primary-black': '#0B101B',
       'primary-grey': '#181E29',
       'primary-lite': '#C9CED6',
-      "white": '#FFFFFF'
-    },
-    dackMode: "class"
+      white: '#FFFFFF',
+      'dark-border': '#353C4A'
+    }
   }
 }
-export const plugins = [];
-
+//add dark mode
+export const darkMode = 'class'
+export const plugins = []
