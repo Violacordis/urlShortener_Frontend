330bc3d14881311f2e147055e2a869ec1d6b899e
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-04 16:14:25 +0100

    add more color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ export const theme = {
       'primary-grey': '#181E29',
       'primary-lite': '#C9CED6',
       white: '#FFFFFF',
-      'dark-border': '#353C4A'
+      'dark-border': '#353C4A',
+      'dark-hd': '#0D1117',
+      'green-acc': '#1EB036',
+      'brown-acc': '#B0901E',
+      'lite-green': 'rgba(30, 176, 54, 0.14)',
+      'lite-brown':"rgba(176, 144, 30, 0.19)",
+      "lite-blue":"rgba(24, 30, 41, 0.22)",
+
+    },
+    backgroundImage: {
+      'dark-mobile': "url('/images/back-mobile.jpg')",
+      'dark-desktop': "url('/images/back-desktop.jpg')",
+      'dark-tablet': "url('/images/back-tablet.jpg')"
     }
   }
 }
