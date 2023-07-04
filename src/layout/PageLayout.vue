daf63fc9eb017189c4169ab0f82d01c67530178e
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-04 20:22:21 +0100

    remove comment, update style

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -23,15 +23,14 @@ const toggleDarkMode = () => {
 <template>
   <div>
     <slot name="header"></slot>
-    <div class="lg">
+    <div>
       <!-- make a toggle button for color theme -->
-      <label class="cursor-pointer flex justify-end mb-3 mr-2">
+      <label class="cursor-pointer flex justify-end my-3 mr-2">
         <div class="relative inline-flex items-center cursor-pointer">
           <input type="checkbox" value="" class="sr-only peer" @click="toggleDarkMode" />
           <div
             class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
           ></div>
-          <!-- <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle me</span> -->
         </div>
       </label>
       <slot name="main"></slot>
