d2c56204926b6bf25cb94c8c5be51acec181fef4
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-04 21:43:33 +0100

    docs: change route

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
-      path:"/register",
+      path:"/signup",
       name:"register",
       component: () => import('../views/SignView.vue')
     }
