76ca4f7d2ffe0fdbf01315efa0dc9b7d78cdff74
Ibimina hart <ibimina.c.hart@gmail.com>
2023-07-04 12:54:47 +0100

    add page loayout

---
+++ src/layout/PageLayout.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+const darkMode = ref(false)
+
+const toggleDarkMode = () => {
+  //check if dark mode is true or false
+  if (darkMode.value) {
+    darkMode.value = false
+    //remove dark mode in html
+    document?.querySelector('html')?.classList.remove('dark')
+
+    console.log('dark mode is false')
+  } else {
+    //if false, set dark mode to true
+    darkMode.value = true
+    //add dark mode in html
+    document?.querySelector('html')?.classList.add('dark')
+    console.log('dark mode is true')
+  }
+}
+</script>
+<template>
+  <div>
+    <slot name="header"></slot>
+    <div class="">
+      <slot name="main"></slot>
+      <button @click="toggleDarkMode">Click me</button>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
+import PageLayout from '@/layout/PageLayout.vue'
 
 
 </script>
 <template>
-  <header class="grid grid-col-5 p-3 md:py-8 md:px-5 gap-1 items-center mb-4">
-    <h1>Logo</h1>
-    <form
-      class="mt-2 row-start-2 col-span-5 border-2 border-current rounded-3xl md:col-start-2 md:col-span-3 md:row-start-1 md:mt-0">
-      <input type="url" placeholder="Enter the link here" class="rounded-3xl w- w-8/12 p-2">
-      <input type="submit" value="Shorten Now" class="col-start-5  w-4/12 rounded-3xl bg-primary-blue text-white p-2">
-    </form>
-    <nav class="col-start-5 justify-self-end">
-      <ul class="flex items-center gap-2">
-        <li>user</li>
-        <li>logout</li>
-      </ul>
-    </nav>
-  </header>
-  <main>
+  <PageLayout>
+    <template #header>
+        <header class="grid grid-col-5 p-3 md:py-8 md:px-5 gap-1 items-center bg-slate-50 dark:bg-primary-black ">
+          <h1 class="dark:text-primary-blue">Logo</h1>
+          <form
+            class="mt-2 row-start-2 col-span-5 border-2 border-current rounded-3xl md:col-start-2 md:col-span-3 md:row-start-1 md:mt-0 bg-slate-50 dark:bg-primary-grey dark:border-dark-border dark:text-primary-lite">
+            <input type="url" placeholder="Enter the link here" class="rounded-3xl outline-none w-8/12 p-2 bg-slate-50 dark:bg-primary-grey">
+            <input type="submit" value="Shorten Now" class="col-start-5  w-4/12 rounded-3xl bg-primary-blue text-white p-2">
+          </form>
+          <nav class="col-start-5 justify-self-end dark:text-primary-pink">
+            <ul class="flex items-center gap-2">
+              <li>user</li>
+              <li>logout</li>
+            </ul>
+          </nav>
+        </header>
+      </template>
+      <template #main>
+       <main>
     <ul class="flex items-center justify-center gap-5 py-4 px-5 bg-slate-700 text-white">
       <li>History</li>
       <li>Statistics</li>
@@ -37,11 +42,11 @@
           <thead class="min-w-full">
             <tr>
               <th scope="col" class="px-6 py-4 whitespace-nowrap">Original Link</th>
-              <th  scope="col" class="px-6 py-4 whitespace-nowrap">Short Link</th>
-              <th  scope="col" class="px-6 py-4 whitespace-nowrap">QR Code</th>
-              <th  scope="col" class="px-6 py-4 whitespace-nowrap">Clicks</th>
-              <th  scope="col" class="px-6 py-4 whitespace-nowrap">Status</th>
-              <th  scope="col" class="px-6 py-4 whitespace-nowrap">Date</th>
+              <th scope="col" class="px-6 py-4 whitespace-nowrap">Short Link</th>
+              <th scope="col" class="px-6 py-4 whitespace-nowrap">QR Code</th>
+              <th scope="col" class="px-6 py-4 whitespace-nowrap">Clicks</th>
+              <th scope="col" class="px-6 py-4 whitespace-nowrap">Status</th>
+              <th scope="col" class="px-6 py-4 whitespace-nowrap">Date</th>
             </tr>
           </thead>
           <tbody>
@@ -53,7 +58,8 @@
                 <a href="https://www.google.com">https://link/ppp</a>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
-                <img class="w-8 mx-auto" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://link/ppp" alt="QR Code">
+                <img class="w-8 mx-auto"
+                  src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://link/ppp" alt="QR Code">
               </td>
               <td class="whitespace-nowrap px-6 py-4">0</td>
               <td class="whitespace-nowrap px-6 py-4">Active</td>
@@ -63,7 +69,14 @@
         </table>
 
       </div>
-      
+
     </section>
   </main>
-</template>
+
+      </template>
+  </PageLayout>
+
+
+
+
+ </template>
